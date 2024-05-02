23f387af5dbe19a204e29c0129bddfbe91210b85
Jesse-L2 <jesse.little@utexas.edu>
2024-05-02 17:07:40 -0500

    Add form cards, inputs,and buttons

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -8,7 +8,37 @@ import './Footer.scss'
 
 
 const Footer = () => {
-  return <div>Footer</div>;
+const [formData, setFormData] = useState({name: '', email: '', message: ''})
+const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+const [loading, setLoading] = useState(false);
+
+  return <>
+  <h2 className="head-text">Chat with me</h2>
+  <div className="app__footer">
+    <div className="app__footer-cards">
+    <div className="app__footer-card">
+      <img src={images.email} alt="email">
+        <a href="mailto:hello@example.com" className="p-text">hello@example.com</a>
+      </img>
+    </div>
+    <div className="app__footer-card">
+      <img src={images.mobile} alt="mobile">
+        <a href="tel:+123456789" className="p-text">123456789</a>
+      </img>
+    </div>
+    </div>
+  </div>
+  <div className="app__footer-form app__flex">
+    <div clasName="app__flex">
+      <input className="p-text" type="text" placeholder="Your Name" name="name" value={name} onChange={handleChangeInput}></input> 
+      <input className="p-text" type="text" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput}></input> 
+    </div>
+    <div>
+      <textarea className="p-text" placeholder="Your Message" name="message" value={message} onChange={handleChangeInput}></textarea>
+    </div>
+    <button className="p-text" type="submit" onClick={handleSubmit}>{loading ? 'loading' : 'Send Message'}</button>
+  </div>
+  </>;
 };
 
 export default AppWrap(
